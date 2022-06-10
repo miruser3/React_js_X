3468c60f301dcaf6d351ee80e07bf425d58a1101
mir <mika___i87mail.ru>
2022-06-10 20:15:06 +0500

    add

--- homework5/my-app/src/App.js
+++ homework5/my-app/src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
 import './App.scss';
+import React, { useEffect, useState, useRef } from "react";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { red } from '@mui/material/colors';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+const theme = createTheme({
+  palette: {
+      primary: {
+          main: red[700],
+      },
+  },
+});
 
 function App() {
+  const [messageList, setMessageList] = useState([]);
+  const ref = useRef(null);
+
+  function handleSubmit(event) {
+      event.preventDefault();
+      const target = event.target;
+      const author = target.author.value;
+      const text = target.text.value;
+
+      setMessageList(prev => [...prev, {
+          id: giveLastId(prev),
+          author: author,
+          text: text,
+      }]);
+
+  }
+
+  function giveLastId(array) {
+      return array.length ? array[array.length - 1].id + 1 : 0;
+  }
+
+  useEffect( () => {
+      setTimeout( () => {
+          botAnswer(messageList);
+      }, 1500 );
+  }, [messageList] );
+
+  function botAnswer() {
+      const lastAuthor = messageList[messageList.length - 1];
+      if (lastAuthor && lastAuthor.author) {
+          setMessageList(prev => [...prev, {
+              id: giveLastId(prev),
+              text: `Сообщение автора ${lastAuthor.author} отправлено`,
+          }]);
+      }
+      ref.current.focus()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+ <ThemeProvider theme={theme}>
+                <div className="container">
+                    <Box component="form" noValidate
+                         autoComplete="off" mt={4} display="flex"
+                         flexDirection="column" onSubmit={handleSubmit}>
+                        <TextField id="name" label="Имя"
+                                   variant="outlined" sx={{ mb: 2 }}
+                                   name="author"
+                                   inputRef={ref} />
+                        <TextField id="message" label="Сообщение"
+                                   variant="outlined" sx={{ mb: 2 }}
+                                   name="text"
+                                   inputRef={ref}/>
+                        <Button variant="outlined" color='primary'
+                                size="small" type="submit" sx={{ mb: 4 }}>Отправить</Button>
+                    </Box>
+                    <div className="message-list">
+                        {messageList.map( message => <div className="message-list__item" key={message.id}>
+                            { message.author && <p className="message-list__p"><span>Автор:</span> {message.author}</p>}
+                            <p className="message-list__p">{ message.author && <span>Текст:</span>} {message.text}</p>
+                        </div> )}
+                    </div>
+                </div>
+
+        </ThemeProvider>
+    );
 }
 
 export default App;
