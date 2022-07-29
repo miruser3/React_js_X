cd249a9d209a762581da159d29f3e4c8b86c316b
mir <mika___i87mail.ru>
2022-07-29 23:26:08 +0500

    add

--- homework_8/app/src/component/App.js
+++ homework_8/app/src/component/App.js
@@ -1,27 +1,39 @@
 import '../App.css';
 import { Routes, Route } from 'react-router-dom';
 import Massege from './Massege';
-import { useEffect, useState } from 'react';
-function App() {
-   const [api,setapi]=useState([]);
 
-  useEffect(()=>{
+
+function App() {
+  //const [api, setapi] = useState([]);
+  /* const getProduct = () => {
+     return fetch("https://jsonplaceholder.typicode.com/comments")
+       .then(response => response.json())
+       .then(data => dispatch({
+         type: GET_PRODUCT,
+         payload: data
+       }))
+       .catch(error => dispatch({
+         type: EROR,
+         payload: error
+       })
+       );
+   }*/
+ 
+  /*useEffect(()=>{
       fetch("https://jsonplaceholder.typicode.com/comments")
       .then((response) => response.json())
       .then((data) => setapi(data));
-},[])
+  },[])*/
 
   return (
     <div className="App">
       <header className="App-header">
-     {api.map((e)=>(
-      <div key={e.id}>{e.name}</div>
-     ))}
+       <Massege/>
 
-     <Routes>
-      <Route path='/' element={<Massege/>}/>
-     </Routes>
-     </header>
+        <Routes>
+          <Route path='/' element={<Massege />} />
+        </Routes>
+      </header>
     </div>
   );
 }
